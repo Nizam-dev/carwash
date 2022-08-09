5293794a667b46f5763a171453e3182e52dcc632
Nizam <mahfudzkhoirunnizam@gmail.com>
2022-08-09 12:57:45 +0700

    perubahan service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('currency', function ( $expression ) { return "Rp. <?php echo number_format($expression,0,',','.'); ?>"; });
         config(['app.locale' => 'id']);
         \Carbon\Carbon::setLocale('id');
+        Schema::defaultStringLength(191);
     }
 }
